67fe62a1f5c6a47da8e3a52dc82dba62cd3998d8
hejinxue001 <hejinxue001@163.com>
2018-08-14 11:44:39 +0800

    consturctor autowired

--- src/main/java/logdb/controller/RedisController.java
+++ src/main/java/logdb/controller/RedisController.java
@@ -11,34 +11,40 @@ public class RedisController {
 
     private ShardedJedisPool shardedJedisPool;
 
-    @Autowired //为啥放到
-    public RedisController(ShardedJedisPool shardedJedisPool) {
+    private JedisPool jedisPool;
+
+    private JedisPoolConfig jedisPoolConfig;
+
+    @Autowired //为啥把注入放到构造器上可以成功注入 放入到方法上就不行?
+    public RedisController(ShardedJedisPool shardedJedisPool, JedisPool jedisPool, JedisPoolConfig jedisPoolConfig) {
         this.shardedJedisPool = shardedJedisPool;
+        this.jedisPool = jedisPool;
+        this.jedisPoolConfig = jedisPoolConfig;
     }
 
     @RequestMapping("/get")
-    public String get(JedisPool jedisPool) {
+    public String get() {
         return jedisPool.getResource().get("test");
     }
 
     @RequestMapping("/get2")
-    public String get() {
+    public String get2() {
         return shardedJedisPool.getResource().get("test");
     }
 
     @RequestMapping("/set")
-    public String set(JedisPool jedisPool) {
+    public String set() {
         return jedisPool.getResource().set("test", "abcde");
     }
 
     @RequestMapping("/config")
-    public JedisPoolConfig config(JedisPoolConfig jedisPoolConfig) {
+    public JedisPoolConfig config() {
         return jedisPoolConfig;
     }
 
     @RequestMapping("/pool")
     @Autowired
-    public JedisPool pool(JedisPool jedisPool) {
+    public JedisPool pool() {
         return jedisPool;
     }
 }
